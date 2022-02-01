dc835c17be0846980b13f6f67dcc332c5b740f53
badru <muhammad.mustaffa.18@ucl.ac.uk>
2022-02-01 21:29:48 +0000

    By Badrul

--- my_app/algorithm/routes.py
+++ my_app/algorithm/routes.py
@@ -2,8 +2,6 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 
 from my_app import db, photos
-from my_app.algorithm.forms import ImageSubmitForm
-from my_app.models import ImageSubmit
 
 algorithm_bp = Blueprint('algorithm_bp', __name__, url_prefix='/algorithm')
 
