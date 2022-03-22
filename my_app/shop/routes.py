39d9164b3fbe912a9088eddfae91df0a85771311
hafiz <zcemmhh@ucl.ac.uk>
2022-03-22 17:05:40 +0000

    by Hafiz

--- my_app/shop/routes.py
+++ my_app/shop/routes.py
@@ -4,9 +4,8 @@ from flask import Blueprint, render_template, request, session, flash, redirect,
 from flask_login import login_required, current_user
 from datetime import datetime
 
-from my_app import db
 from my_app.shop.forms import ShoppingCartForm
-
+from my_app import db
 from my_app.models import ShoppingCart
 
 shop_bp = Blueprint('shop_bp', __name__, url_prefix='/shop')
@@ -20,14 +19,16 @@ def product():
 
     form = ShoppingCartForm()
     # cart = ShoppingCart.query.filter_by(username=name).first()
-    cart = session.get('cart', {'kit':0,'machine':0})
-
+    cart = session.get('cart', {'kit': 0, 'machine': 0})
 
     if request.method =='POST':
         item = request.form.get('itemid')
         cart[item] += 1
         flash('Your shopping cart has been updated!')
         session['cart'] = cart
+
+    return render_template('shop_product.html', form=form, name=name, cart=cart)
+
     # if request.method == 'POST' and form.validate():
     #     session['item'] = request.form['itemA']
     #     session['item'] = request.form['itemB']
@@ -39,28 +40,56 @@ def product():
         #
 
         # else:
-        #     # addtocart = ShoppingCart(username=name, QuantityA=form.QuantityA.data, QuantityB=form.QuantityB.data)
-        #     # db.session.add(addtocart)
-        #     # db.session.commit()
-        #     # flash('Item added to your Shopping Cart!')
-        #     # return redirect(url_for('shop_bp.product'))
-
-    return render_template('shop_product.html', item=cart, form=form, name=name, cart=cart)
+        #     addtocart = ShoppingCart(username=name, QuantityA=form.QuantityA.data, QuantityB=form.QuantityB.data)
+        #     db.session.add(addtocart)
+        #     db.session.commit()
+        #     flash('Item added to your Shopping Cart!')
+        #     return redirect(url_for('shop_bp.product'))
 
 
 
-@shop_bp.route('/payment', defaults={'name': 'traveler'})
+
+@shop_bp.route('/payment', defaults={'name': 'traveler'}, methods=['GET', 'POST'])
 # @login_required
 def payment(name):
+
     if not current_user.is_anonymous:
         name = current_user.username
 
-    return render_template('shop_payment.html', name=name)
+    if session.get('cart', None) is not None:
+        cart = session.get('cart')
+        session['cart'] = cart
+        kitqty = session['cart']['kit']
+        machqty = session['cart']['machine']
+        kitprice = kitqty*5
+        machprice = machqty*100
+        totalprice = machprice + kitprice
+
+    return render_template('shop_payment.html', name=name, cart=cart, kitprice=kitprice, machprice=machprice, totalprice=totalprice)
+
 
 @shop_bp.route('/clear')
 def clear():
-    session.pop('cart',default = None)
-    flash('shopping cart emptied')
-    return render_template('shop_product.html')
+    session.pop('cart', default=None)
+    flash('Shopping cart emptied!')
+    return redirect("/shop/product")
 
+@shop_bp.route('/checkout', defaults={'name': 'traveler'})
+def checkout(name):
+    if not current_user.is_anonymous:
+        name = current_user.username
 
+    if session.get('cart', None) is not None:
+        cart = session.get('cart')
+        session['cart'] = cart
+        kitqty = session['cart']['kit']
+        machqty = session['cart']['machine']
+
+        purchase = ShoppingCart(username=name, QuantityA=kitqty, QuantityB=machqty)
+        db.session.add(purchase)
+        db.session.commit()
+
+        session.pop('cart', default=None)
+
+    flash('purchase confirmed!')
+    return redirect(url_for('shop_bp.product'))
