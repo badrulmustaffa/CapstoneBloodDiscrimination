0f75789a744d27072ca683a54396ea3f69efb84d
farhan mirza <zcemfmb@ucl.ac.uk>
2022-05-11 23:24:46 +0100

    images added by wong

--- my_app/algorithm/routes.py
+++ my_app/algorithm/routes.py
@@ -18,7 +18,7 @@ algorithm_bp = Blueprint('algorithm_bp', __name__, url_prefix='/algorithm')
 
 #json_file = open(url_for('algorithm', filename='model.json'),'r')
 #json_file = open('model.json','r')
-model_json_path = "C:\\Users\\hafiz\\PycharmProjects\\CapstoneBloodDiscrimination\\my_app\\algorithm\\model.json"
+model_json_path = "C:\\Users\\farha\\PycharmProjects\\CapstoneBloodDiscrimination\\my_app\\algorithm\\model.json"
 json_file = open(model_json_path, 'r')
 
 loaded_model_json = json_file.read()
@@ -29,7 +29,7 @@ json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 
 # load weights into new model
-model_h5_path = "C:\\Users\\hafiz\\PycharmProjects\\CapstoneBloodDiscrimination\\my_app\\algorithm\\model.h5"
+model_h5_path = "C:\\Users\\farha\\PycharmProjects\\CapstoneBloodDiscrimination\\my_app\\algorithm\\model.h5"
 loaded_model.load_weights(model_h5_path)
 print("Loaded Model from disk")
 
