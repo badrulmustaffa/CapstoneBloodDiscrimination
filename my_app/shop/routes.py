8b1bae22df29dce764abab38cab94ebf96d01dc5
hafiz <zcemmhh@ucl.ac.uk>
2022-05-11 13:10:36 +0100

    by Hafiz

--- my_app/shop/routes.py
+++ my_app/shop/routes.py
@@ -21,7 +21,7 @@ def product():
     # cart = ShoppingCart.query.filter_by(username=name).first()
     cart = session.get('cart', {'kit': 0, 'machine': 0})
 
-    if request.method =='POST':
+    if request.method == 'POST':
         item = request.form.get('itemid')
         cart[item] += 1
         flash('Your shopping cart has been updated!')
@@ -42,11 +42,18 @@ def payment(name):
         session['cart'] = cart
         kitqty = session['cart']['kit']
         machqty = session['cart']['machine']
-        kitprice = kitqty*5
+        kitprice = round(kitqty*9.85,2)
         machprice = machqty*100
-        totalprice = machprice + kitprice
+        totalprice = round((machprice + kitprice),2)
+        tax = round(0.2*totalprice,2)
 
-    return render_template('shop_payment.html', name=name, cart=cart, kitprice=kitprice, machprice=machprice, totalprice=totalprice)
+    return render_template('shop_payment.html',
+                           name=name,
+                           cart=cart,
+                           kitprice=kitprice,
+                           machprice=machprice,
+                           totalprice=totalprice,
+                           tax=tax)
 
 
 @shop_bp.route('/clear')
@@ -54,6 +61,7 @@ def clear():
     session.pop('cart', default=None)
     flash('Shopping cart emptied!')
     return redirect("/shop/product")
+
 
 @shop_bp.route('/checkout', defaults={'name': 'traveler'})
 def checkout(name):
@@ -67,11 +75,13 @@ def checkout(name):
         machqty = session['cart']['machine']
         kitamount = kitqty
         ID = 0
-        refnum = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))+'-'+''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+        refnum = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))\
+                 + '-'\
+                 + ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
 
         while ID < kitamount:
             kit_ID = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-            ID = ID +1
+            ID = ID + 1
 
             registerID = KitID(username=name,
                                kitID=kit_ID,
@@ -90,16 +100,22 @@ def checkout(name):
 
         session.pop('cart', default=None)
 
-    flash('purchase confirmed!')
-    return redirect(url_for('shop_bp.confirm', ref_num=refnum))
+    flash('Purchase confirmed!')
+    # return redirect(url_for('shop_bp.confirm', refnum=refnum))
+    return render_template('shop_confirm.html', refnum=refnum)
+
 
 @shop_bp.route('/gen_kitid')
 def gen_kitid():
     # function to generate kid ID
-    result_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))+'-'+''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+    result_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))\
+                 + '-'\
+                 + ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
     return redirect(url_for('shop_bp.product', result_str=result_str))
 
 
 @shop_bp.route('/confirm')
 def confirm():
-    return render_template('shop_confirm.html')
+    # refnum = session.get('refnum')
+    # reference = ShoppingCart.query.filter_by(ref_num=refnum).all()
+    return render_template('shop_confirm.html', reference=reference)
